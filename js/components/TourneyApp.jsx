a891798af627bcc2c2c8eb74a97f125d9d16dc99
Alex Odle <aodle@palantir.com>
2015-04-10 18:28:07 -0400

    simple worst scores of the day list

--- js/components/TourneyApp.jsx
+++ js/components/TourneyApp.jsx
@@ -3,15 +3,21 @@
 
 var _ = require('lodash');
 var ChatRoom = require('./ChatRoom.jsx');
+var constants = require('../../common/constants');
 var GolfDraftPanel = require('./GolfDraftPanel.jsx');
+var GolferStore = require('../stores/GolferStore');
 var LogoutButton = require('./LogoutButton.jsx');
 var moment = require('moment');
 var PlayerDetails = require('./PlayerDetails.jsx');
 var PlayerStandings = require('./PlayerStandings.jsx');
 var React = require('react');
 var ScoreLogic = require('../logic/ScoreLogic');
+var utils = require('../../common/utils');
 
 var ReactPropTypes = React.PropTypes;
+
+var NDAYS = constants.NDAYS;
+var MISSED_CUT = constants.MISSED_CUT;
 
 function getState(state, props) {
   return {
@@ -38,13 +44,38 @@ var TourneyApp = React.createClass({
       this.props.scores
     );
 
+    var scores = this.props.scores;
+    var worstScoresPerDay = _.chain(NDAYS)
+      .times(function (day) {
+        var worstScore = _.chain(scores)
+          .reject(function (s) {
+            return s.scores[day] === MISSED_CUT;
+          })
+          .max(function (s) {
+            return s.scores[day];
+          })
+          .value();
+        return {
+          day: day,
+          golfer: worstScore.golfer,
+          score: worstScore.scores[day]
+        };
+      })
+      .first(function (s) {
+        // Assume 0 means they haven't started playing this day yet
+        return s.score > 0;
+      })
+      .value();
+
     return (
       <section>
-
-        <GolfDraftPanel heading='Score Details'>
+        <p>
           <small>
             Scores sync every 10 minutes. Last sync: <b>{moment(this.props.lastScoresUpdate).calendar()}</b>
           </small>
+        </p>
+
+        <GolfDraftPanel heading='Score Details'>
           <PlayerDetails
             player={this.state.playerDetailsPlayer}
             playerScores={playerScores}
@@ -52,13 +83,27 @@ var TourneyApp = React.createClass({
           />
         </GolfDraftPanel>
 
+        {!worstScoresPerDay.length ? null : (
+          <GolfDraftPanel heading='Worst Scores of the Day'>
+            <ul>
+              {_.map(worstScoresPerDay, function (s) {
+                return (
+                  <li key={s.day} className='list-unstyled'>
+                    <b>Day {s.day + 1}</b>: {utils.toGolferScoreStr(s.score)} {GolferStore.getGolfer(s.golfer).name}
+                  </li>
+                );
+              })}
+            </ul>
+          </GolfDraftPanel>
+        )}
+
         <GolfDraftPanel heading='Overall Standings'>
-            <PlayerStandings
-              currentUser={this.props.currentUser}
-              playerScores={playerScores}
-              selectedPlayer={this.state.playerDetailsPlayer}
-              onPlayerSelect={this._onPlayerSelect}
-            />
+          <PlayerStandings
+            currentUser={this.props.currentUser}
+            playerScores={playerScores}
+            selectedPlayer={this.state.playerDetailsPlayer}
+            onPlayerSelect={this._onPlayerSelect}
+          />
         </GolfDraftPanel>
 
         <ChatRoom messages={this.props.chatMessages} />
