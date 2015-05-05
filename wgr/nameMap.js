33d061f4954ba99434538d2936577525e52ff49f
Alex Odle <aodle@palantir.com>
2015-05-05 15:00:40 -0400

    more config updates

--- wgr/nameMap.js
+++ wgr/nameMap.js
@@ -1,6 +1,14 @@
 // Map names from WGR site to what they are in the Yahoo site
+//
+// <wgr site>: <yahoo site>
+//
 module.exports = {
   'Sangmoon Bae': 'Sang-Moon Bae',
   'Seungyul Noh': 'Seung-yul Noh',
-  'Jose M Olazabal': 'Jose Maria Olazabal'
+  'Jose M Olazabal': 'Jose Maria Olazabal',
+  'Graham Delaet': 'Graham DeLaet',
+  'Gonzalo Fdez-Castano': 'Gonzalo Fernandez-Casta',
+  'Charles Howell-III': 'Charles Howell III',
+  'Billy Hurley-III': 'Billy Hurley III',
+  'Will Mackenzie': 'Will MacKenzie'
 };
