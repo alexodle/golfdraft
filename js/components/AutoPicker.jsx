0ae276c1582265f3fb41259852bd7704d1f0b41b
Alex Odle <aodle@palantir.com>
2015-06-25 15:04:07 -0400

    separate available and pick order

--- js/components/AutoPicker.jsx
+++ js/components/AutoPicker.jsx
@@ -2,28 +2,49 @@
 'use strict';
 
 var _ = require('lodash');
-var React = require('react');
-var GolfDraftPanel = require('./GolfDraftPanel.jsx');
 var AutoPickActions = require('../actions/AutoPickActions');
+var GolfDraftPanel = require('./GolfDraftPanel.jsx');
+var GolferStore = require('../stores/GolferStore');
+var React = require('react');
 
 var AutoPicker = React.createClass({
 
   render: function () {
-    var golfersRemaining = this.props.golfersRemaining;
-    var autoPickOrder = this.props.autoPickOrder;
+    var golfersRemaining = _.chain(this.props.golfersRemaining)
+      .sortBy('wgr')
+      .pluck('id')
+      .value();
     var isAutoPick = this.props.isAutoPick;
+    var autoPickOrder = _.intersection(this.props.autoPickOrder, golfersRemaining);
 
     return (
       <GolfDraftPanel heading='Auto Picking'>
-        <div>
-          autoPickOrder: {JSON.stringify(autoPickOrder)}<br />
-          isAutoPick: {isAutoPick.toString()}
-        </div>
-
         <div className='checkbox'>
           <label>
             <input type='checkbox' value={isAutoPick} onChange={this._setIsAutoPick} /> Auto-pick
           </label>
+        </div>
+
+        <div className='row'>
+
+          <div className='col-md-6'>
+            <GolfDraftPanel heading='Pick Order'>
+              {JSON.stringify(autoPickOrder)}
+            </GolfDraftPanel>
+          </div>
+
+          <div className='col-md-6'>
+            <GolfDraftPanel heading='Available Players'>
+              <ol>
+                {_.map(golfersRemaining, function (g) {
+                  return (
+                    <li key={g}>{GolferStore.getGolfer(g).name}</li>
+                  );
+                })}
+              </ol>
+            </GolfDraftPanel>
+          </div>
+
         </div>
       </GolfDraftPanel>
     );
