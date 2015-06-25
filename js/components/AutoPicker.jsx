2ab2b0cf203cf91e7710c5539644a8fe20f226d8
Alex Odle <aodle@palantir.com>
2015-06-25 17:00:38 -0400

    moveable players in the auto pick list

--- js/components/AutoPicker.jsx
+++ js/components/AutoPicker.jsx
@@ -7,6 +7,20 @@ var GolfDraftPanel = require('./GolfDraftPanel.jsx');
 var GolferStore = require('../stores/GolferStore');
 var React = require('react');
 
+function getGolferFromClickEvent(ev) {
+  ev.preventDefault();
+  return ev.target.dataset.golfer;
+}
+
+function safeSwap(arr, iFrom, iTo) {
+  if (iTo < 0 || iTo >= arr.length) return arr;
+
+  var temp = arr[iTo];
+  arr[iTo] = arr[iFrom];
+  arr[iFrom] = temp;
+  return arr;
+}
+
 var AutoPicker = React.createClass({
 
   render: function () {
@@ -20,9 +34,15 @@ var AutoPicker = React.createClass({
     var golfersAvailable = _.difference(golfersRemaining, autoPickOrder);
 
     var moveToPickOrder = this._moveToPickOrder;
+    var movePlayerUp = this._movePlayerUp;
+    var movePlayerDown = this._movePlayerDown;
+    var moveToAvailable = this._moveToAvailable;
 
     return (
       <GolfDraftPanel heading='Auto Picking'>
+        {!isAutoPick || _.isEmpty(autoPickOrder) ? null : (
+          <p>Next pick will be: <b>{GolferStore.getGolfer(autoPickOrder[0]).name}</b></p>
+        )}
         <div className='checkbox'>
           <label>
             <input type='checkbox' value={isAutoPick} onChange={this._setIsAutoPick} /> Auto-pick
@@ -36,7 +56,15 @@ var AutoPicker = React.createClass({
               <ol>
                 {_.map(autoPickOrder, function (g) {
                   return (
-                    <li key={g}>{GolferStore.getGolfer(g).name}</li>
+                    <li key={g}>
+                      {GolferStore.getGolfer(g).name}
+                      <span> </span>
+                      <a data-golfer={g} href='#' onClick={movePlayerUp}>Up</a>
+                      <span> </span>
+                      <a data-golfer={g} href='#' onClick={movePlayerDown}>Down</a>
+                      <span> </span>
+                      <a data-golfer={g} href='#' onClick={moveToAvailable}>X</a>
+                    </li>
                   );
                 })}
               </ol>
@@ -50,8 +78,9 @@ var AutoPicker = React.createClass({
                   return (
                     <li key={g}>
                       <a
+                        data-golfer={g}
                         href='#'
-                        onClick={_.partial(moveToPickOrder, g)}
+                        onClick={moveToPickOrder}
                       >{GolferStore.getGolfer(g).name}
                       </a>
                     </li>
@@ -70,8 +99,31 @@ var AutoPicker = React.createClass({
     AutoPickActions.setIsAutoPick(ev.target.checked);
   },
 
-  _moveToPickOrder: function (g) {
+  _moveToPickOrder: function (ev) {
+    var g = getGolferFromClickEvent(ev);
     var autoPickOrder = this.props.autoPickOrder.concat([g]);
+    AutoPickActions.setAutoPickOrder(autoPickOrder);
+  },
+
+  _moveToAvailable: function (ev) {
+    var g = getGolferFromClickEvent(ev);
+    var autoPickOrder = _.without(this.props.autoPickOrder, g);
+    AutoPickActions.setAutoPickOrder(autoPickOrder);
+  },
+
+  _movePlayerUp: function (ev) {
+    var g = getGolferFromClickEvent(ev);
+    var index = _.indexOf(this.props.autoPickOrder, g);
+
+    var autoPickOrder = safeSwap(this.props.autoPickOrder, index, index - 1);
+    AutoPickActions.setAutoPickOrder(autoPickOrder);
+  },
+
+  movePlayerDown: function (ev) {
+    var g = getGolferFromClickEvent(ev);
+    var index = _.indexOf(this.props.autoPickOrder, g);
+
+    var autoPickOrder = safeSwap(this.props.autoPickOrder, index, index + 1);
     AutoPickActions.setAutoPickOrder(autoPickOrder);
   }
 
