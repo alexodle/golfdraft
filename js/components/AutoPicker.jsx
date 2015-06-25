5b39289f587c5eeb8f54970d270a01f9b84c5abb
Alex Odle <aodle@palantir.com>
2015-06-25 16:35:35 -0400

    moveable players

--- js/components/AutoPicker.jsx
+++ js/components/AutoPicker.jsx
@@ -14,8 +14,12 @@ var AutoPicker = React.createClass({
       .sortBy('wgr')
       .pluck('id')
       .value();
+
     var isAutoPick = this.props.isAutoPick;
     var autoPickOrder = _.intersection(this.props.autoPickOrder, golfersRemaining);
+    var golfersAvailable = _.difference(golfersRemaining, autoPickOrder);
+
+    var moveToPickOrder = this._moveToPickOrder;
 
     return (
       <GolfDraftPanel heading='Auto Picking'>
@@ -29,16 +33,28 @@ var AutoPicker = React.createClass({
 
           <div className='col-md-6'>
             <GolfDraftPanel heading='Pick Order'>
-              {JSON.stringify(autoPickOrder)}
+              <ol>
+                {_.map(autoPickOrder, function (g) {
+                  return (
+                    <li key={g}>{GolferStore.getGolfer(g).name}</li>
+                  );
+                })}
+              </ol>
             </GolfDraftPanel>
           </div>
 
           <div className='col-md-6'>
             <GolfDraftPanel heading='Available Players'>
               <ol>
-                {_.map(golfersRemaining, function (g) {
+                {_.map(golfersAvailable, function (g) {
                   return (
-                    <li key={g}>{GolferStore.getGolfer(g).name}</li>
+                    <li key={g}>
+                      <a
+                        href='#'
+                        onClick={_.partial(moveToPickOrder, g)}
+                      >{GolferStore.getGolfer(g).name}
+                      </a>
+                    </li>
                   );
                 })}
               </ol>
@@ -52,6 +68,11 @@ var AutoPicker = React.createClass({
 
   _setIsAutoPick: function (ev) {
     AutoPickActions.setIsAutoPick(ev.target.checked);
+  },
+
+  _moveToPickOrder: function (g) {
+    var autoPickOrder = this.props.autoPickOrder.concat([g]);
+    AutoPickActions.setAutoPickOrder(autoPickOrder);
   }
 
 });
