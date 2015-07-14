104739f6b726f7ce876dc947b63e271ce9d4e6bb
Alex Odle <aodle@palantir.com>
2015-07-14 16:22:00 -0400

    update player config

--- poolPlayerConfig.js
+++ poolPlayerConfig.js
@@ -1,22 +1,45 @@
+// http://www.pgatour.com/data/r/100/leaderboard-v2.json
+
+/*
+1 - Kyle
+2 - Graham
+3 - Hennessey
+4 - Severson
+5 - Jon M
+6 - Rob
+7 - Odle
+8 - Robes
+9 - Kevin
+10 - Stacy
+11 - Frank
+12 - Ben F
+13 - Jay
+14 - Jon W
+15 - Chad
+16 - Welch
+17 - Matteus
+18 - Chris
+*/
+
 module.exports = {
   draftOrder: [
-    'Jared Robillard',
-    'Matt Simon',
-    'Stacy Smith',
-    'Erik Severson',
-    'Graham Snead',
-    'Bo Welch',
-    'Jonathan Mullen',
-    'Frank Severinghaus',
-    'Jay Fredlund',
-    'Kevin Phillips',
-    'Rob Phillips',
-    'Chris Willett',
-    'Ben Frizzel',
-    'Matt Hennessey',
-    'Alex Odle',
-    'Chad Larson',
-    'Jon Willett',
     'Kyle Phillips',
+    'Graham Snead',
+    'Matt Hennessey',
+    'Erik Severson',
+    'Jonathan Mullen',
+    'Rob Phillips',
+    'Alex Odle',
+    'Jared Robillard',
+    'Kevin Phillips',
+    'Stacy Smith',
+    'Frank Severinghaus',
+    'Ben Frizzel',
+    'Jay Fredlund',
+    'Jon Willett',
+    'Chad Larson',
+    'Bo Welch',
+    'Matt Simon',
+    'Chris Willett',
   ]
 };
