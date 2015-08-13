463a03ec24ced5aa7a45d72e13f6c95de38351bc
Alex Odle <aodle@palantir.com>
2015-08-13 13:32:13 -0400

    re-bootstrap if someone multi-connects (phone fix hopefully)

--- js/startSocketUpdates.js
+++ js/startSocketUpdates.js
@@ -1,10 +1,16 @@
+'use strict';
+
+var $ = require('jquery');
 var _ = require('lodash');
+var AppActions = require('./actions/AppActions');
 var ChatActions = require('./actions/ChatActions');
 var DraftActions = require('./actions/DraftActions');
 var ScoreActions = require('./actions/ScoreActions');
 var SettingsActions = require('./actions/SettingsActions');
 var socketio = require('socket.io-client');
 var UserActions = require('./actions/UserActions');
+
+var hasConnected = false;
 
 /** Starts listening for app-wide socket.io updates
 */
@@ -29,6 +35,32 @@ function startSocketUpdates() {
   // ADMIN power
   io.on('action:forcerefresh', function (ev) {
     window.location.reload();
+  });
+
+  // Force refresh if it's been sufficiently long since our last update
+  io.on('connect', function () {
+    if (!hasConnected) {
+      hasConnected = true;
+    } else {
+
+      // TODO - Move to separate server sync
+      $.get('/bootstrap')
+      .fail(function () {
+        // No real error handling here, just reload the page to make sure we
+        // don't get people in a weird state.
+        window.location.reload();
+      })
+      .success(function (results) {
+        AppActions.setPlayers(results.players);
+        AppActions.setGolfers(results.golfers);
+        DraftActions.draftUpdate(results.draft);
+        ScoreActions.scoreUpdate({
+          scores: results.scores,
+          lastUpdated: results.tourney.lastUpdated
+        });
+        SettingsActions.setIsPaused(results.appState.isDraftPaused);
+      });
+    }
   });
 }
 
