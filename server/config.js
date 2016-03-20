2961a0cb0c40e88bca57e5fe0de797ac8e1c42ec
Alex Odle <aodle@palantir.com>
2016-03-20 15:31:16 -0400

    new mongo var

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 var cfg = {
   prod: !process.env.DEBUG,
 
-  mongo_url: process.env.MONGOHQ_URL || "mongodb://localhost:27017/test",
+  mongo_url: process.env.MONGOLAB_URI || "mongodb://localhost:27017/test",
   redis_url: process.env.REDISTOGO_URL || "redis://:@localhost:6379/0",
 
   tourney_id: process.env.TOURNEY_ID || '5376879322ed79dd19a07148',
