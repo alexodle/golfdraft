0bf6f073ebe243ce7edc31cae51132251d5132fe
Alex Odle <aodle@palantir.com>
2016-03-31 16:34:50 -0400

    remove old comments

--- poolPlayerConfig.js
+++ poolPlayerConfig.js
@@ -1,27 +1,6 @@
 // 2015 PGA Championship
 // http://www.pgatour.com/data/r/033/leaderboard-v2mini.json
 
-/*
-1 - Graham
-2 - Bo
-3 - Matteus
-4 - Stacy
-5 - Robes
-6 - Frank
-7 - Ben
-8 - Kevin
-9 - Severson
-10 - Rob
-11 - Chad
-12 - Chris
-13 - Jon M
-14 - Odle
-15 - Kyle
-16 - Jon W
-17 - Jay
-18 - Henny
-*/
-
 module.exports = {
   draftOrder: [
     'Graham Snead',
