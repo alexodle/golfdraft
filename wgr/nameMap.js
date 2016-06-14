30e741411c68d177d844cfcfdefacf82d780341a
Alex Odle <aodle@palantir.com>
2016-06-14 09:35:40 -0400

    update name map

--- wgr/nameMap.js
+++ wgr/nameMap.js
@@ -3,15 +3,19 @@
 // <wgr site>: <pga tour site>
 //
 module.exports = {
+  'Alexander Noren': 'Alex Noren',
+  'Andy Sullivan': 'Andrew Sullivan',
   'Billy Hurley-III': 'Billy Hurley III',
-  'Byeong Hun An': 'Byeong-Hun An',
   'Charles Howell-III': 'Charles Howell III',
   'Graham Delaet': 'Graham DeLaet',
   'Jin Cheng': 'Cheng Jin',
   'Liang Wen-chong': 'Wen-Chong Liang',
+  'Matthew Fitzpatrick': 'Matt Fitzpatrick',
+  'Maximilian Kieffer': 'Max Kieffer',
   'Miguel A Jimenez': 'Miguel Angel Jimenez',
   'Rafael Cabrera Bello': 'Rafa Cabrera Bello',
   'Seukhyun Baek': 'Seuk Hyun Baek',
   'Siwoo Kim': 'Si Woo Kim',
-  'Tjaart Van Der Walt': 'Tjaart van der Walt'
+  'Sunghoon Kang': 'Sung Kang',
+  'Tjaart Van Der Walt': 'Tjaart van der Walt',
 };
