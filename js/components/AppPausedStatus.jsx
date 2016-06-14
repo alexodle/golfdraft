280891e1fb895685361f76095bf3adc4605b75f6
Alex Odle <aodle@palantir.com>
2016-06-14 10:09:13 -0400

    pause update

--- js/components/AppPausedStatus.jsx
+++ js/components/AppPausedStatus.jsx
@@ -9,9 +9,31 @@ var AppPausedStatus = React.createClass({
     return (
       <div className="jumbotron">
         <h1>Pause!</h1>
+
         <p>
-          The draft is on pause while I figure some shit out. Thanks for being patient!<br />
-          - Alex
+          <em><small>noun</small></em>
+          <ol>
+            <li>
+              <span>a temporary stop in action or speech.</span><br />
+              <span className='dict-definition'>
+                "she dropped me outside during a brief pause in the rain"<br />
+                <em>synonyms:</em> stop, cessation, break, halt, interruption, check, lull, respite, breathing space, discontinuation, hiatus, gap, interlude;
+              </span>
+            </li>
+          </ol>
+        </p>
+
+        <p>
+          <small>verb</small>
+          <ol>
+            <li>
+              <span>interrupt action or speech briefly.</span><br />
+              <span className='dict-definition'>
+                "she paused, at a loss for words"<br />
+                <em>synonyms:</em> stop, cease, halt, discontinue, break off, take a break;
+              </span>
+            </li>
+          </ol>
         </p>
       </div>
     );
