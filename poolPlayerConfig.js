690f0fcc3f3047186083668fe0cecbb7fd52a9bf
Alex Odle <aodle@palantir.com>
2016-07-26 08:07:55 -0400

    16pga

--- poolPlayerConfig.js
+++ poolPlayerConfig.js
@@ -1,50 +1,50 @@
-// 2016 British Open
-// scores: http://www.pgatour.com/data/r/100/leaderboard-v2mini.json
+// 2016 PGA Championship
+// scores: http://www.pgatour.com/data/r/033/leaderboard-v2mini.json
 // field:
 //
 // Order:
 /*
-1 - Henny
-2 - Odle
-3 - Kyle
-4 - Jon M
-5 - Frank
-6 - Bo
-7 - Matt S
-8 - Chad
-9 - Stacy
-10 - Kevin
-11 - Ben
-12 - Chris W
-13 - Rob
-14 - Robes
-15 - Jay
-16 - Jon W
-17 - Adam
-18 - Severson
-19 - Graham
+1 - Jon W
+2 - Severson
+3 - Frank
+4 - Rob
+5 - Graham
+6 - Jon M
+7 - Chad
+8 - Henny
+9 - Bo
+10 - Kyle
+11 - Adam
+12 - Stacy
+13 - Kevin
+14 - Odle
+15 - Ben
+16 - Robes
+17 - Chris
+18 - Matt S
+19 - Jay
 */
 
 module.exports = {
   draftOrder: [
-    'Matt Hennessey',
-    'Alex Odle',
-    'Kyle Phillips',
-    'Jonathan Mullen',
+    'Jon Willett',
+    'Erik Severson',
     'Frank Severinghaus',
-    'Bo Welch',
-    'Matt Simon',
+    'Rob Phillips',
+    'Graham Snead',
+    'Jonathan Mullen',
     'Chad Larson',
+    'Matt Hennessey',
+    'Bo Welch',
+    'Kyle Phillips',
+    'Adam Morrison',
     'Stacy Smith',
     'Kevin Phillips',
+    'Alex Odle',
     'Ben Frizzel',
-    'Chris Willett',
-    'Rob Phillips',
     'Jared Robillard',
-    'Jay Fredlund',
-    'Jon Willett',
-    'Adam Morrison',
-    'Erik Severson',
-    'Graham Snead'
+    'Chris Willett',
+    'Matt Simon',
+    'Jay Fredlund'
   ]
 };
--- wgr/nameMap.js
+++ wgr/nameMap.js
@@ -3,12 +3,10 @@
 // <wgr site>: <pga tour site>
 //
 module.exports = {
-  'Alexander Noren': 'Alex Noren',
+  'Alex Noren': 'Alexander Noren',
   'Billy Hurley-III': 'Billy Hurley III',
-  'Byeong Hun An': 'Byeong-Hun An',
   'Charles Howell-III': 'Charles Howell III',
   'Graham Delaet': 'Graham DeLaet',
-  'Jeunghun Wang': 'Jeung-hun Wang',
   'Jin Cheng': 'Cheng Jin',
   'Liang Wen-chong': 'Wen-Chong Liang',
   'Maximilian Kieffer': 'Max Kieffer',
@@ -20,9 +18,13 @@ module.exports = {
   'Siwoo Kim': 'Si Woo Kim',
   'Steven Alker': 'Steve Alker',
   'Sunghoon Kang': 'Sung Kang',
-  'Tjaart Van Der Walt': 'Tjaart van der Walt'
+  'Tjaart Van Der Walt': 'Tjaart van der Walt',
+  'Younghan Song': 'Young-han Song'
 
-  // No longer valid for 2016 British Open
+  // No longer valid for 2016 PGA Championship
   // 'Andy Sullivan': 'Andrew Sullivan',
   // 'Matthew Fitzpatrick': 'Matt Fitzpatrick',
+  // 'Alexander Noren': 'Alex Noren',
+  // 'Byeong Hun An': 'Byeong-Hun An',
+  // 'Jeunghun Wang': 'Jeung-hun Wang',
 };
