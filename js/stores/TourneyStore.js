2f19f983d5768552be866b3f341abe775ee1a8e5
Alex Odle <aodle@palantir.com>
2017-04-05 21:36:17 -0400

    update tourney name

--- js/stores/TourneyStore.js
+++ js/stores/TourneyStore.js
@@ -7,7 +7,7 @@ var TourneyStore =  _.extend({}, Store.prototype, {
 
   getTourneyName: function () {
     // HACKHACK - hardcode for now
-    return "2016 PGA Championship";
+    return "2017 Masters";
   }
 
 });
