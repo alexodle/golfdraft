9ff0cef4591b6c9d26846d60a7c4b4e7c86d8420
Alex Odle <aodle@palantir.com>
2017-07-18 12:18:11 -0700

    fix name map

--- wgr/nameMap.js
+++ wgr/nameMap.js
@@ -5,28 +5,32 @@
 module.exports = {
   'Billy Hurley-III': 'Billy Hurley III',
   'Charles Howell-III': 'Charles Howell III',
+  'Giwhan Kim': 'Gi-whan Kim',
   'Graham Delaet': 'Graham DeLaet',
   'Jin Cheng': 'Cheng Jin',
+  'Joseph Dean': 'Joe Dean',
+  'Juan Sebastian Munoz': 'Sebastian Munoz',
   'Liang Wen-chong': 'Wen-Chong Liang',
   'Maximilian Kieffer': 'Max Kieffer',
   'Miguel A Jimenez': 'Miguel Angel Jimenez',
   'Rafael Cabrera Bello': 'Rafa Cabrera Bello',
   'Sanghee Lee': 'Sang-hee Lee',
   'Seukhyun Baek': 'Seuk Hyun Baek',
+  'Seungyul Noh': 'Seung-Yul Noh',
   'Siwoo Kim': 'Si Woo Kim',
   'Steven Alker': 'Steve Alker',
   'Sunghoon Kang': 'Sung Kang',
   'Tjaart Van Der Walt': 'Tjaart van der Walt',
-  'Younghan Song': 'Young-han Song',
-  'Jeunghun Wang': 'Jeung-hun Wang',
   'Tyrone van Aswegen': 'Tyrone Van Aswegen',
-  'Seungyul Noh': 'Seung-Yul Noh'
+  'Yikeun Chang': 'Yi Keun Chang'
 
-  // No longer valid for 2017 US Open
+  // No longer valid for 2017 British Open
   // 'Byeong Hun An': 'Byeong-Hun An',
   // 'Alex Noren': 'Alexander Noren',
   // 'Andy Sullivan': 'Andrew Sullivan',
   // 'Matthew Fitzpatrick': 'Matt Fitzpatrick',
   // 'Alexander Noren': 'Alex Noren',
   // 'Seungyul Noh': 'Seung-yul Noh',
+  // 'Jeunghun Wang': 'Jeung-hun Wang',
+  // 'Younghan Song': 'Young-han Song',
 };
