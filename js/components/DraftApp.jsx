0254601b0e9958cbe784f814c53ceb953d091e51
Alex Odle <aodle@palantir.com>
2018-01-02 12:50:52 -0500

    simplify

--- js/components/DraftApp.jsx
+++ js/components/DraftApp.jsx
@@ -19,26 +19,11 @@ var SettingsActions = require("../actions/SettingsActions");
 var myTurnSound = new Audio(Assets.MY_TURN_SOUND);
 var pickMadeSound = new Audio(Assets.PICK_MADE_SOUND);
 
-var TABS = keyMirror({
-  "DRAFT_HQ": null,
-  "PICK_LIST_EDITOR": null
-});
-
-var TAB_ORDER = [TABS.DRAFT_HQ, TABS.PICK_LIST_EDITOR];
-
-var TAB_DISPLAY_NAMES = {
-  "DRAFT_HQ": "Draft HQ",
-  "PICK_LIST_EDITOR": "Pick Priorities"
-}
-
-var DEFAULT_TAB = TABS.DRAFT_HQ;
-
 var DraftApp = React.createClass({
 
   getInitialState: function () {
     return {
-      draftHistoryPlayerId: null,
-      tabSelection: DEFAULT_TAB
+      draftHistoryPlayerId: null
     };
   },
 
@@ -54,56 +39,32 @@ var DraftApp = React.createClass({
   },
 
   render: function () {
-    var tabSelection = this.state.tabSelection;
-
-    return (
-      <div>
-        <div className="row">
-          <div className="col-md-12">
-            <ul className="nav nav-tabs" style={{marginBottom: "20px"}}>
-              {_.map(TAB_ORDER, function (t) {
-                return (
-                  <li
-                    key={t}
-                    role="presentation"
-                    onClick={this._onTabClick.bind(this, t)}
-                    className={tabSelection == t ? "active" : ""}
-                  >
-                    <a href="#">{TAB_DISPLAY_NAMES[t]}</a>
-                  </li>
-                );
-              }, this)}
-            </ul>
-          </div>
-        </div>
-        {this._renderTabSubview()}
-      </div>
-    );
-  },
-
-  _renderTabSubview: function () {
-    var tabSelection = this.state.tabSelection;
     var isMyPick = this.props.isMyDraftPick;
     var isDraftPaused = this.props.isPaused;
 
-    if (tabSelection == TABS.DRAFT_HQ) {
-      var priorityPanelHeader = (
-        <span>
-          <a href="#" onClick={this._onTabClick.bind(this, TABS.PICK_LIST_EDITOR)}>
-            <span className="glyphicon glyphicon-new-window"/>
-          </a>
-          <span>&nbsp;&nbsp;</span>
-          <span>Pick Priorities</span>
-          <span className="pull-right"><em>NEW!</em></span>
-        </span>
-      );
-      return (
+    var priorityPanelHeader = (
+      <span>
+        <span>Pick Priorities</span>
+        <span className="pull-right"><em>NEW!</em></span>
+      </span>
+    );
+
+    return (
+      <div>
         <div>
           {isDraftPaused ? (<AppPausedStatus />) : (
             <div className="row">
               <div className="col-md-9">
                 {!isMyPick ? (
-                  <DraftStatus currentPick={this.props.currentPick} />
+                  <div>
+                    <DraftStatus currentPick={this.props.currentPick} />
+                    <DraftPickOrder
+                      currentUser={this.props.currentUser}
+                      currentPick={this.props.currentPick}
+                      pickingForPlayers={this.props.pickingForPlayers}
+                      onPlayerSelected={this._onDraftHistorySelectionChange}
+                    />
+                  </div>
                 ) : (
                   <DraftChooser
                     currentUser={this.props.currentUser}
@@ -124,23 +85,6 @@ var DraftApp = React.createClass({
           )}
           <div className="row">
             <div className="col-md-12">
-              <ChatRoom
-                currentUser={this.props.currentUser}
-                messages={this.props.chatMessages}
-                activeUsers={this.props.activeUsers}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">
-              <DraftPickOrder
-                currentUser={this.props.currentUser}
-                currentPick={this.props.currentPick}
-                pickingForPlayers={this.props.pickingForPlayers}
-                onPlayerSelected={this._onDraftHistorySelectionChange}
-              />
-            </div>
-            <div className="col-md-6">
               <GolfDraftPanel heading={priorityPanelHeader}>
                 <PickListEditor
                   syncedPriority={this.props.syncedPriority}
@@ -148,6 +92,15 @@ var DraftApp = React.createClass({
                   height="29em"
                 />
               </GolfDraftPanel>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <ChatRoom
+                currentUser={this.props.currentUser}
+                messages={this.props.chatMessages}
+                activeUsers={this.props.activeUsers}
+              />
             </div>
           </div>
           <div className="row">
@@ -160,38 +113,12 @@ var DraftApp = React.createClass({
             </div>
           </div>
         </div>
-      );
-    } else { // if (tabSelection == TABS.PICK_LIST_EDITOR) {
-      return (
-        <div>
-          <div className="row">
-            <div className="col-md-12">
-              <GolfDraftPanel heading="Pick Priorities">
-                <PickListEditor
-                  syncedPriority={this.props.syncedPriority}
-                  pendingPriority={this.props.pendingPriority}
-                />
-              </GolfDraftPanel>
-            </div>
-          </div>
-        </div>
-      );
-    }
-  },
-
-  _onPriorityEdit: function () {
-    window.scrollTo(0, 0);
-    this.setState({ draftHistoryPlayerId: playerId, requestAutoPriorityEdit: true });
+      </div>
+    );
   },
 
   _onDraftHistorySelectionChange: function (playerId) {
     this.setState({ draftHistoryPlayerId: playerId });
-  },
-
-  _onTabClick: function (t) {
-    if (t != this.state.tabSelection) {
-      this.setState({ tabSelection: t });
-    }
   }
 
 });
