f6c4543008027ad94dcf1ffe0c9070cf65c5a0db
Alex Odle <aodle@palantir.com>
2018-01-05 10:08:54 -0500

    babel; webpack; grunt++

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,7 @@ module.exports = function (grunt) {
   require('time-grunt')(grunt);
 
   const webpack = require('webpack');
+  const path = require('path');
 
   grunt.initConfig({
     express: {
@@ -34,13 +35,12 @@ module.exports = function (grunt) {
 
       prod: {
         output: {
-          path: './dist/',
+          path: path.resolve(__dirname, 'dist'),
           filename: 'bundle.js'
         },
         plugins: [
-          new webpack.optimize.DedupePlugin(),
           new webpack.optimize.UglifyJsPlugin(),
-          new webpack.optimize.OccurenceOrderPlugin(),
+          new webpack.optimize.OccurrenceOrderPlugin(),
           new webpack.optimize.AggressiveMergingPlugin(),
           new webpack.DefinePlugin({
             // Remove all debug-only code from React
@@ -53,7 +53,7 @@ module.exports = function (grunt) {
 
       dev: {
         output: {
-          path: './distd/',
+          path: path.resolve(__dirname, 'distd'),
           filename: 'bundle.js'
         },
         //devtool: 'eval', // Fast rebuild
@@ -102,6 +102,7 @@ module.exports = function (grunt) {
     }
 
   });
+  grunt.loadNpmTasks('grunt-webpack');
 
   grunt.registerTask('test', [
     'mochaTest'
