f818400d6dbad6e34b33ddb01ced366ae1e4e8ea
Ubuntu <ubuntu@ip-172-31-37-8.us-east-2.compute.internal>
2018-01-05 16:40:29 +0000

    script++

--- ec2_config.sh
+++ ec2_config.sh
@@ -6,8 +6,12 @@ export TOURNEY_CFG="/home/ec2-user/golfraft_cfg/tourney_cfg.json"
 unset DEBUG
 export PORT=8000
 
-export MONGO_URI="mongodb://gd:<password>@127.0.0.1:27017/gd"
-export REDISTOGO_URL=redis://redistogo:<password>@telescopefish.redistogo.com:9422/
+# NEED TO FILL IN
+export REDIS_PW=<password>
+export MONGO_PW=<password
+
+export MONGO_URI="mongodb://gd:${MONGO_PW}@127.0.0.1:27017/gd"
+export REDISTOGO_URL=redis://redistogo:{$REDIS_PW}@telescopefish.redistogo.com:9422/
 
 export TOURNEY_ID=596e402a829d03944426f355
 
