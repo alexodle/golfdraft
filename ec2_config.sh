c845371474296457af6f74c884ec1488ac54de33
Ubuntu <ubuntu@ip-172-31-37-8.us-east-2.compute.internal>
2018-01-05 16:42:22 +0000

    script++

--- ec2_config.sh
+++ ec2_config.sh
@@ -7,11 +7,10 @@ unset DEBUG
 export PORT=8000
 
 # NEED TO FILL IN
-export REDIS_PW=<password>
-export MONGO_PW=<password
+export REDIS_TOGO_URL=TODO
+export MONGO_PW=TODO
 
 export MONGO_URI="mongodb://gd:${MONGO_PW}@127.0.0.1:27017/gd"
-export REDISTOGO_URL=redis://redistogo:{$REDIS_PW}@telescopefish.redistogo.com:9422/
 
 export TOURNEY_ID=596e402a829d03944426f355
 
