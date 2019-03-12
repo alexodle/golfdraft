a72fddec9551b08a3afb08a36c6a197480632e14
Ubuntu <ubuntu@ip-172-31-45-50.us-east-2.compute.internal>
2019-03-12 17:32:11 +0000

    ensure to run buildServer before starting server

--- ec2_runServer.sh
+++ ec2_runServer.sh
@@ -1,18 +1,17 @@
-#!/bin/bash
+#!/bin/bash -e
 
 ## Idea is to run this on a screen session
 
-BASE_DIR=~/golfdraft
+killall node || true
 
-killall node
-
-source $BASE_DIR/../config.sh
+source ~/config.sh
 
 pushd $BASE_DIR
 git checkout master
 git pull origin master
 npm install
 
+npm run buildServer
 npm start > $BASE_DIR/../serverlog.log 2>&1 &
 
 popd
