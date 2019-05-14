7a27dbbaa43e83adeaf3e0f053a6f35805d2d694
Alex Odle <alex.odle@gmail.com>
2019-05-14 10:30:14 -0700

    use async for wgr parser

--- wgr/rawWgrReader.ts
+++ wgr/rawWgrReader.ts
@@ -5,36 +5,34 @@ import * as request from 'request';
 
 const AMATEUR_REGEX = /\(Am\)$/i;
 
-export function rawWgrReader(url: string): Promise<WGR[]> {
-  return new Promise(function (fulfill, reject) {
-    request({ url }, function (error, response, body) {
+function download(url: string): Promise<string> {
+  return new Promise((fulfill, reject) => {
+    request({ url }, (error: Error, _response: Response, body: string) => {
       if (error) {
         reject(error);
         return;
       }
-
-      let dom: JSDOM = null;
-      try {
-        dom = new JSDOM(body);
-      } catch (err) {
-        reject(err);
-        return;
-      }
-
-      const wgrs: WGR[] = [];
-
-      const trs = dom.window.document.body.querySelectorAll('#ranking_table > .table_container > table > tbody > tr');
-      trs.forEach(tr => {
-        const tds = tr.querySelectorAll('td');
-        const wgr = _.parseInt(tds.item(0).textContent);
-        const name = tr.querySelector('td.name')
-          .textContent
-          .trim()
-          .replace(AMATEUR_REGEX, '');
-        wgrs.push({ wgr, name } as WGR);
-      });
-
-      fulfill(wgrs);
+      fulfill(body);
     });
   });
+}
+
+export async function rawWgrReader(url: string): Promise<WGR[]> {
+  const wgrs: WGR[] = [];
+
+  const body = await download(url);
+  const dom = new JSDOM(body);
+
+  const trs = dom.window.document.body.querySelectorAll('#ranking_table > .table_container > table > tbody > tr');
+  trs.forEach(tr => {
+    const tds = tr.querySelectorAll('td');
+    const wgr = _.parseInt(tds.item(0).textContent);
+    const name = tr.querySelector('td.name')
+      .textContent
+      .trim()
+      .replace(AMATEUR_REGEX, '');
+    wgrs.push({ wgr, name } as WGR);
+  });
+  
+  return wgrs;
 }
