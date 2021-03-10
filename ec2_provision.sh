af61f30f820f90149e11984a732bd4f138cc00be
Alex Odle <Alex.Odle@cloudkitchens.com>
2021-03-09 21:04:32 -0800

    include scraping app

--- ec2_provision.sh
+++ ec2_provision.sh
@@ -4,9 +4,13 @@ BASE_DIR=~/golfdraft
 
 source $BASE_DIR/../config.sh
 
+apt-get update
+apt-get install -y libgbm-dev
+
 $BASE_DIR/ec2_installNode.sh
 
 $BASE_DIR/ec2_installMongodb.sh
 $BASE_DIR/initializeMongo.sh
 
 $BASE_DIR/ec2_installNginx.sh
+
